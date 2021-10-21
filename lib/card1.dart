d3bf6e8e57a764e5d04cb080d12de85043e50c85
Mwita04 <emcee779@gmail.com>
2021-10-21 21:53:00 +0300

    commit on master

--- lib/card1.dart
+++ lib/card1.dart
@@ -20,6 +20,41 @@ class Card1 extends StatelessWidget {
       // TODO: Card1 Decorate Container
       child: Container(
         // TODO: Add a stack of text
+        child: Stack(
+          children: [
+            // 8
+            Text(
+              category,
+              style: FooderlichTheme.darkTextTheme.bodyText1,
+            ),
+            // 9
+            Positioned(
+              child: Text(
+                title,
+                style: FooderlichTheme.darkTextTheme.headline2,
+              ),
+              top: 20,
+            ),
+            // 10
+            Positioned(
+              child: Text(
+                description,
+                style: FooderlichTheme.darkTextTheme.bodyText1,
+              ),
+              bottom: 30,
+              right: 0,
+            ),
+            // 11
+            Positioned(
+              child: Text(
+                chef,
+                style: FooderlichTheme.darkTextTheme.bodyText1,
+              ),
+              bottom: 10,
+              right: 0,
+            )
+          ],
+        ),
 
         // 1
         padding: const EdgeInsets.all(16),
