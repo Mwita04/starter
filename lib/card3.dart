f1346ec1748a9629d6d98674fa091aba8130f19c
Mwita04 <emcee779@gmail.com>
2021-10-21 22:51:07 +0300

    commit on master

--- lib/card3.dart
+++ lib/card3.dart
@@ -29,9 +29,98 @@ class Card3 extends StatelessWidget {
                 borderRadius: const BorderRadius.all(Radius.circular(10.0)),
               ),
             ),
-
-            // TODO 6: Add Container, Column, Icon and Text
-            // TODO 7: Add Center widget with Chip widget children
+            Container(
+              // 3
+              padding: const EdgeInsets.all(16),
+              // 4
+              child: Column(
+                // 5
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // 6
+                  const Icon(
+                    Icons.book,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                  // 7
+                  const SizedBox(height: 8),
+                  // 8
+                  Text(
+                    'Recipe Trends',
+                    style: FooderlichTheme.darkTextTheme.headline2,
+                  ),
+                  // 9
+                  const SizedBox(height: 30),
+                ],
+              ),
+            ),
+            Center(
+              // 11
+              child: Wrap(
+                // 12
+                alignment: WrapAlignment.start,
+                // 13
+                spacing: 12,
+                // 14
+                children: [
+                  Chip(
+                    label: Text('Healthy',
+                        style: FooderlichTheme.darkTextTheme.bodyText1),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                    onDeleted: () {
+                      print('delete');
+                    },
+                  ),
+                  Chip(
+                    label: Text('Vegan',
+                        style: FooderlichTheme.darkTextTheme.bodyText1),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                    onDeleted: () {
+                      print('delete');
+                    },
+                  ),
+                  Chip(
+                    label: Text('Carrots',
+                        style: FooderlichTheme.darkTextTheme.bodyText1),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                  ),
+                  Center(
+                    // 11
+                    child: Wrap(
+                      // 12
+                      alignment: WrapAlignment.start,
+                      // 13
+                      spacing: 12,
+                      // 14
+                      children: [
+                        Chip(
+                          label: Text('Healthy',
+                              style: FooderlichTheme.darkTextTheme.bodyText1),
+                          backgroundColor: Colors.black.withOpacity(0.7),
+                          onDeleted: () {
+                            print('delete');
+                          },
+                        ),
+                        Chip(
+                          label: Text('Vegan',
+                              style: FooderlichTheme.darkTextTheme.bodyText1),
+                          backgroundColor: Colors.black.withOpacity(0.7),
+                          onDeleted: () {
+                            print('delete');
+                          },
+                        ),
+                        Chip(
+                          label: Text('Carrots',
+                              style: FooderlichTheme.darkTextTheme.bodyText1),
+                          backgroundColor: Colors.black.withOpacity(0.7),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
